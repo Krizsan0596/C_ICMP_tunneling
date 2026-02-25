f530f5d1f822ce1541ae6a2cb5a5b3a84eb99f5e
Krizsan0596 <kristof.nagy.lazar@proton.me>
2026-02-25 13:39:50 +0100

    feat(build): add CMake support and refactor sources
    Introduce CMakeLists.txt for build automation. Move source files into
    lib/ and src/ directories for better organization. Update .gitignore to
    exclude build artifacts. Implement server entry point in src/server.c
    and update includes for new structure.

---
+++ src/server.c
@@ -0,0 +1,48 @@
+#include <bits/types/sigset_t.h>
+#include <pthread.h>
+#include <stdatomic.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include "../lib/sender.h"
+
+volatile _Atomic program_state state = RUNNING;
+
+void handle_sigint(int sig){
+    state = ABORT;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: ./%s INPUT_FILENAME DESTINATION_IP", argv[0]);
+    }
+
+    sigset_t set;
+    struct sigaction sig;
+
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    pthread_sigmask(SIG_BLOCK, &set, NULL);
+
+    pthread_t producer_thread;
+    thread_args *producer_args = malloc(sizeof(thread_args));
+    producer_args->file = argv[1];
+    producer_args->dest_ip = argv[2];
+    
+    pthread_create(&producer_thread, NULL, start_thread, producer_args);
+    ssize_t *file_size;
+
+    sig.sa_handler = &handle_sigint;
+    sig.sa_flags = 0;
+    sigemptyset(&sig.sa_mask);
+    sigaction(SIGINT, &sig, NULL);
+
+    pthread_sigmask(SIG_UNBLOCK, &set, NULL);
+
+    while (state != ABORT && state != FINISHED) {
+        pthread_join(producer_thread, (void **)&file_size);
+    }
+
+    return 0;
+}
